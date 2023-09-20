6564d219e6ed16aaeea429f79c6947d4005578c9
ciel <cielchang0324@gmail.com>
2023-09-20 18:24:44 +0800

    copy week6 and push to week7 github repo

---
+++ main.js
@@ -0,0 +1,30 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+console.log("Hello world!");
+
+// 讓index__recent__cards 卡片片尾的狀態區(開賣中...檢視活動etc)可以有hover跳動效果
+let stateIcons = document.querySelectorAll('.card-footer__state');
+stateIcons.forEach((item) => {
+  item.addEventListener('mouseover', function () {
+    this.lastElementChild.classList.add('fa-beat-fade');
+    // console.log(this.lastElementChild);
+  })
+  item.addEventListener('mouseleave', function () {
+    this.lastElementChild.classList.remove('fa-beat-fade');
+  })
+})
+// index__追蹤訂閱的位置button的hover效果
+let subscribeBtn = document.querySelector('.index__followAndSubscribe .wrap2 [type="submit"]');
+subscribeBtn.addEventListener('mouseover', function () {
+  this.lastElementChild.classList.add('fa-beat-fade');
+})
+subscribeBtn.addEventListener('mouseleave', function () {
+  this.lastElementChild.classList.remove('fa-beat-fade');
+})
+let tests=document.querySelectorAll('.fa-angles-right');
+tests.forEach((item)=>{
+  item.parentElement.addEventListener('mouseover',function(e){
+    console.log(this.children);
+  })
+})
